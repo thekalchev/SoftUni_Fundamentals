61b9b5e9c927237ae78bf66933ff970349ba4e87
Dimitar Kalchev <131408520+thekalchev@users.noreply.github.com>
2023-10-02 01:09:10 +0300

    lists

--- Lists Basics - Exercise/Survival_of_the_Biggest.py
+++ Lists Basics - Exercise/Survival_of_the_Biggest.py
@@ -3,8 +3,7 @@ list_of_numbers = list(map(int, list_of_numbers))
 numbers_to_remove = int(input())
 
 for i in range(numbers_to_remove):
-    if list_of_numbers:
-        list_of_numbers.remove(min(list_of_numbers))
+    list_of_numbers.remove(min(list_of_numbers))
 
 output = ', '.join(map(str, list_of_numbers))
 print(output)
