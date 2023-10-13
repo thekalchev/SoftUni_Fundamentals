69dd06e658070599e253857a7367756e590e3815
Dimitar Kalchev <131408520+thekalchev@users.noreply.github.com>
2023-10-14 01:13:50 +0300

    f

--- Functions - Exercise/Even_Numbers.py
+++ Functions - Exercise/Even_Numbers.py
@@ -1,8 +1,17 @@
-sequence = input().split()
-sequence_as_even_integers = []
-for i in sequence:
-    num = int(i)
-    if num % 2 == 0:
-        sequence_as_even_integers.append(num)
-print(sequence_as_even_integers)
+# sequence = input().split()
+# sequence_as_even_integers = []
+# for i in sequence:
+#     num = int(i)
+#     if num % 2 == 0:
+#         sequence_as_even_integers.append(num)
+# print(sequence_as_even_integers)
+#
+
+# num_as_string = input().split()
+# num_as_digits = []
+# for num in num_as_string:
+#     num_as_digits.append(int(num))
+# is_even = lambda x: x % 2 == 0
+# result = list(filter(is_even, num_as_digits))
+# print(result)
 
