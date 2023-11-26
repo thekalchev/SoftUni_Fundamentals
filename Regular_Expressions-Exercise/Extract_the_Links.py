70e99d556958cf4ad9d38628acfc023df933309b
Dimitar Kalchev <131408520+thekalchev@users.noreply.github.com>
2023-11-26 19:44:58 +0200

    Regular Expressions

---
+++ Regular_Expressions-Exercise/Extract_the_Links.py
@@ -0,0 +1,9 @@
+import re
+line = input()
+pattern = r'(w{3}\.[A-Za-z0-9\-\.]+\.[a-z]+)'
+while line:
+    match = re.search(pattern, line)
+    if match:
+        valid_url = match.group(1)
+        print(valid_url)
+    line = input()
